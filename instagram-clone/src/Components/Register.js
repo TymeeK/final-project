c53f99468891d7b35f354c13c5d34580f3677fa9
Tymee Kong <tymeekong562@gmail.com>
2022-04-21 19:42:24 -0700

    Added divs around label and input

--- instagram-clone/src/Components/Register.js
+++ instagram-clone/src/Components/Register.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { MainContainer } from '../Styling/Login.Style';
+import { MainContainer, H1 } from '../Styling/Login.Style';
 
 export default function Register() {
     return (
         <div>
             <MainContainer>
                 <form action=''>
-                    <label htmlFor='email'>Email</label>
-                    <input type='text' name='email' />
-                    <label htmlFor='password'>Password</label>
-                    <input type='text' name='password' />
-                    <label htmlFor='confirmpassword'>Confirm Password</label>
-                    <input type='text' name='confirmpassword' />
+                    <H1>Fakestagram</H1>
+                    <div>
+                        <label htmlFor='email'>Email</label>
+                        <input type='text' name='email' />
+                    </div>
+                    <div>
+                        <label htmlFor='password'>Password</label>
+                        <input type='text' name='password' />
+                    </div>
+                    <div>
+                        <label htmlFor='confirmpassword'>
+                            Confirm Password
+                        </label>
+                        <input type='text' name='confirmpassword' />
+                    </div>
                 </form>
             </MainContainer>
         </div>
