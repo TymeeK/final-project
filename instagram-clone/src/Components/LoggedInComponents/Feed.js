3df78409a34bac4056756e4aff9b24f6ec6bc5fa
Tymee Kong <tymeekong562@gmail.com>
2022-04-27 21:44:24 -0700

    Made navbar yellow

--- instagram-clone/src/Components/LoggedInComponents/Feed.js
+++ instagram-clone/src/Components/LoggedInComponents/Feed.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { H1 } from '../../Styling/Login.Style';
-import { NavBar } from '../../Styling/Feed.Style';
+import { HeadingDiv, NavBar } from '../../Styling/Feed.Style';
 
 export default function Feed() {
     return (
         <>
             <NavBar>
                 <H1>Fakestagram</H1>
+                <input type='text' />
+                <button>Profile</button>
             </NavBar>
+            <div></div>
         </>
     );
 }
--- instagram-clone/src/Styling/Feed.Style.js
+++ instagram-clone/src/Styling/Feed.Style.js
@@ -2,4 +2,6 @@ import styled from 'styled-components';
 
 export const NavBar = styled.nav`
     display: flex;
+    background-color: yellow;
+    justify-content: space-between;
 `;
