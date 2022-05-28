2636fb4c49de244179eaa0fc887ff18dcd22d4e4
Tymee Kong <tymeekong562@gmail.com>
2022-05-28 12:50:50 -0700

    Images now show up on feed page

--- instagram-clone/src/Components/LoggedInComponents/Feed.js
+++ instagram-clone/src/Components/LoggedInComponents/Feed.js
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { H1 } from '../../Styling/Login.Style';
-import { NavBar, ImageContainer } from '../../Styling/Feed.Style';
+import {
+    NavBar,
+    ImageContainer,
+    ImageDiv,
+    FeedImage,
+    PostContainer,
+} from '../../Styling/Feed.Style';
 import { useNavigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase-config';
 
 export default function Feed() {
     //Show profile page
     const navigate = useNavigate();
+    const [feedImages, setFeedImages] = useState([]);
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        function getUser() {
+            const auth = getAuth();
+            onAuthStateChanged(auth, user => {
+                if (user) {
+                    const userId = user.email;
+                    setUser(userId);
+                } else {
+                    console.log('Logged out');
+                }
+            });
+        }
+
+        async function getDocuments() {
+            const querySnapshot = await getDocs(collection(db, 'users'));
+            querySnapshot.forEach(doc => {
+                const posts = doc.data().posts;
+                posts.forEach(element => {
+                    setFeedImages(images => [...images, element.picUrl]);
+                });
+            });
+        }
+
+        getUser();
+        getDocuments();
+    }, []);
     return (
         <>
             <NavBar>
                 <H1>Fakestagram</H1>
-                <button
+                <H1
                     onClick={() => navigate('/profile')}
                     style={{ cursor: 'pointer' }}
                 >
-                    Profile
-                </button>
+                    {user}
+                </H1>
             </NavBar>
             <ImageContainer>
-                <div>
-                    <img src='' alt='placeholder' />
-                </div>
+                {feedImages.map((element, id) => {
+                    return (
+                        <PostContainer>
+                            <div>
+                                <label style={{ display: 'block' }}>
+                                    {user} posted this
+                                </label>
+                            </div>
+                            <ImageDiv key={id}>
+                                <FeedImage src={element} alt='feedimage' />{' '}
+                            </ImageDiv>
+                        </PostContainer>
+                    );
+                })}
             </ImageContainer>
         </>
     );
--- instagram-clone/src/Styling/Feed.Style.js
+++ instagram-clone/src/Styling/Feed.Style.js
@@ -12,4 +12,31 @@ export const ImageContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
+`;
+
+export const PostContainer = styled.div`
+    /* outline: 1px solid red; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 900px;
+    height: 700px;
+`;
+export const ImageDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 500px;
+    margin-top: 30px;
+    outline: 1px solid black;
+`;
+
+export const FeedImage = styled.img`
+    /* object-fit: cover; */
+    min-width: 100%;
+    min-height: 100%;
 `;
